b5a978400d208b23532f562e81f31ef827697080
ForeverZer0 <efreed09@gmail.com>
2022-11-17 02:08:37 -0500

    Initial commit

---
+++ Gemfile
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+source "https://rubygems.org"
+
+# Specify your gem's dependencies in modrinth.gemspec
+gemspec
+
+gem "rake", "~> 13.0"
---
+++ lib/modrinth.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "modrinth/version"
+
+module Modrinth
+  class Error < StandardError; end
+  # Your code goes here...
+end
